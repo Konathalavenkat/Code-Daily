78bfb376fba2090db257b9e89c0faa49b1617ea4
venkat <venkatsivaff@gmail.com>
2024-11-14 16:23:21 +0530

    November 14 2023

---
+++ Leetcode/November_2024/Nov_14.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        def ispossible(mid):
+            curr=0
+            for i in quantities:
+                curr+=math.ceil(i/mid)
+            return curr<=n
+        l,r=1,max(quantities)
+        while(l<=r):
+            mid=(l+r)//2
+            if(ispossible(mid)):
+                r=mid-1
+            else:
+                l=mid+1
+        return r+1
