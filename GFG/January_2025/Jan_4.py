f957f40774bc748e771e229a07b80c3026e20c39
venkat <venkatsivaff@gmail.com>
2025-01-04 23:53:18 +0530

    January 4 2025

---
+++ GFG/January_2025/Jan_4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countTriplets(self, arr, target):
+        from collections import defaultdict
+        m = defaultdict(int)
+        for e in arr:
+            m[e] += 1
+        
+        ans = 0
+        for i in range(len(arr)):
+            m[arr[i]] -= 1             
+            for j in range(0, i):  
+                lookfor = target-arr[i]-arr[j]
+                ans += m[lookfor]
+        return ans
